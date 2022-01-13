58b9e16dd316029e791fd6e170e92683aed9b019
Vinesse <vverning@gmail.com>
2022-01-12 22:29:07 -0800

    incl comments for each function

--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
     "net/http"
 )
 
+// function to handle all requests to our root url
 func homePage(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Welcome to the homepage!")
     fmt.Println("Endpoint hit: homepage")
 }
 
+// function to match URL path hit with a defined function
 func handleRequests() {
     http.HandleFunc("/", homePage)
     log.Fatal(http.ListenAndServe(":1000", nil))
 }
 
+// main function
 func main() {
     handleRequests()
 }
