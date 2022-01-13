6a403143ca068f650bc8fde9c1dc24eca417dbf9
Vinesse <vverning@gmail.com>
2022-01-12 22:21:42 -0800

    include gitignore

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ import (
     "net/http"
 )
 
-func homepage(w http.ResponseWriter, r *http.Request){
-    
+func homePage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome to the homepage!")
+    fmt.Println("Endpoint hit: homepage")
 }
 
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":1000", nil))
+}
+
+func main() {
+    handleRequests()
+}
